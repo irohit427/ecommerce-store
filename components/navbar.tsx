6090c6267fd381b945d36633672858fdd5cc14eb
Rohit <irohit427@users.noreply.github.com>
2023-09-05 23:11:21 +0530

    Added Logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import MainNavigation from "@/components/mainNavigation";
 import { Category } from "@/types";
 import NavbarActions from "./navbarActions";
 import getStore from "@/actions/getStore";
+import Image from "next/image";
 
 const Navbar = async () => {
   const categories: Category[] = await getCategories();
@@ -15,7 +16,10 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">{store.name}</p>
+            <div className="flex flex-row justify-center items-center">
+              <Image src="/logo.png" alt="logo" width={36} height={36} />
+              <p className="font-bold text-xl">{store.name}</p>
+            </div>
           </Link>
           <MainNavigation data={categories} />
           <NavbarActions />
